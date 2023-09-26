791ccbc54bd67ed7ac508746554d828af133b182
Gareth Davidson <gaz@bitplane.net>
2023-09-26 20:01:32 +0100

    lets try it

---
+++ experiments/polyglot.ps1
@@ -0,0 +1,43 @@
+#!/bin/bash
+: <<'POWERSHELL'
+<#
+.SYNOPSIS
+This is a polyglot script that detects the platform.
+#>
+
+# Detect Windows architecture
+$arch = if ([IntPtr]::Size -eq 8) { "amd64" } else { "386" }
+
+# Detect if Windows is ARM
+if ((Get-WmiObject -Class Win32_Processor).Architecture -eq 5) {
+    $arch = "arm"
+} elseif ((Get-WmiObject -Class Win32_Processor).Architecture -eq 12) {
+    $arch = "arm64"
+}
+
+Write-Host "windows-$arch"
+
+# Exit from PowerShell to prevent running bash part
+exit
+#>
+POWERSHELL
+
+# Bash code starts here. It will be ignored in PowerShell due to <# ... #> comment block
+
+# Detect Linux/Mac and architecture
+platform="unknown"
+case $(uname -s) in
+    Linux*) platform="linux" ;;
+    Darwin*) platform="darwin" ;;
+esac
+
+arch="unknown"
+case $(uname -m) in
+    x86_64) arch="amd64" ;;
+    i386) arch="386" ;;
+    armv7*) arch="arm" ;;
+    aarch64) arch="arm64" ;;
+esac
+
+echo "$platform-$arch"
+
