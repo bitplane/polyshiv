06a6e71b19536db0307b0ad89f20c4281519dddc
Gareth Davidson <gaz@bitplane.net>
2023-09-26 20:55:12 +0100

    okay lets try llamasoft approach

--- experiments/polyglot.ps1
+++ experiments/polyglot.ps1
@@ -1,10 +1,8 @@
 #!/bin/bash
+echo \" <<'POWERSHELL_SCRIPT' >/dev/null # " | Out-Null
 
-# This bit runs in Windows
-: '
-powershell -Command {
 
-    $POLYGLOT_MODE="POWERSHELL"
+# ===== PowerShell Script Begin =====
 
     # Detect Windows architecture
     $arch = if ([IntPtr]::Size -eq 8) { "amd64" } else { "386" }
@@ -18,33 +16,34 @@ powershell -Command {
 
     Write-Host "windows-$arch"
 
-    # Exit from PowerShell to prevent running bash part
-    exit
-}
-'
+# ====== PowerShell Script End ======
 
-false @"
-false "
-# This bit runs on Unices
 
-# Detect Linux/Mac and architecture
-platform="unknown"
-case $(uname -s) in
-    Linux*) platform="linux" ;;
-    Darwin*) platform="darwin" ;;
-esac
-
-arch="unknown"
-case $(uname -m) in
-    x86_64) arch="amd64" ;;
-    i386) arch="386" ;;
-    armv7*) arch="arm" ;;
-    aarch64) arch="arm64" ;;
-esac
-
-echo "$platform-$arch"
-
+while ( ! $MyInvocation.MyCommand.Source ) { $input_line = Read-Host }
 exit
+<#
+POWERSHELL_SCRIPT
+set +o histexpand 2>/dev/null
+# ===== Bash Script Begin =====
 
-"@
+    # Detect Linux/Mac and architecture
+    platform="unknown"
+    case $(uname -s) in
+        Linux*) platform="linux" ;;
+        Darwin*) platform="darwin" ;;
+    esac
 
+    arch="unknown"
+    case $(uname -m) in
+        x86_64) arch="amd64" ;;
+        i386) arch="386" ;;
+        armv7*) arch="arm" ;;
+        aarch64) arch="arm64" ;;
+    esac
+
+    echo "$platform-$arch"
+
+# ====== Bash Script End ======
+case $- in *"i"*) cat /dev/stdin >/dev/null ;; esac
+exit
+#>
